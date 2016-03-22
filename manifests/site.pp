8c69f00b39a6987abd4535e62deef8559f05ca56
VVP <vishnuvpotty@gmail.com>
2016-03-22 09:09:05 +0530

    updated content for exec

--- manifests/site.pp
+++ manifests/site.pp
@@ -52,7 +52,7 @@ node default {
   # content => 'Hello test file manage in puppet',
   # }
   
-  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+  exec { 'Hi testing exec commands.' > /etc/motd:
        path => '/usr/bin:/usr/local/bin',
        creates => '/etc/motd',
        }
