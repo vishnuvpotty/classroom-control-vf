8b8640130dcb5e995c2848de99adfebf4b1df4b9
VVP <vishnuvpotty@gmail.com>
2016-03-23 14:37:05 +0530

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,22 +1,51 @@
 class nginx {
 
+case$::osfamily {
+'redhat','debian' : {
+
+$package = 'nginx'
+$fileowner= 'root'
+$group= 'root'
+$docroot='/var/www'
+$configdir = '/etc/nginx'
+$serverlogs ='/var/logs/nginx'
+}
+
+'windows' : {
+$package = 'nginx-service'
+$fileowner = 'Administrator'
+$group = 'Administrators'
+$docroot = 'C:/ProgramData/nginx/html'
+$configdir = 'C:/ProgramData/nginx'
+$serverlogs ='C:/ProgramData/nginx/logs'
+}
+'default' : {
+fail("Module is not supported on ${::osfamily}")
+}
+}
+
+$user= $::osfamily? {
+'redhat' =>'nginx',
+'debian' => 'www-data',
+'windows' =>'nobody',
+}
+
+
+
 File{
-group => root,
-owner => root,
+group => $group,
+owner => $fileowner,
 mode => '0664',
 }
 
-package {'nginx':
+package {$package:
 ensure => present,
 }
-
-$docroot='/var/www';
 
 #document root /var/www
 file {$docroot:
 ensure => directory,
 }
-
 
 #index.html
 file{"${docroot}/index.html":
@@ -25,26 +54,27 @@ source => 'puppet:///modules/nginx/index.html',
 }
 
 #config file nginx.conf
-file {'/etc/nginx/nginx.conf':
+file {"${configdir}/nginx.conf':
 ensure => file,
-
-source => 'puppet:///modules/nginx/nginx.conf',
-require => Package['nginx'],
+content => template('nginx/nginx.conf.erb'),
+# source => 'puppet:///modules/nginx/nginx.conf',
+# require => Package['nginx'],
 notify => Service['nginx'],
 }
 
-file { '/etc/nginx/conf.d':
-ensure => directory,
-owner => 'root',
-group => 'root',
-mode => '0775',
-}
+# file { '/etc/nginx/conf.d':
+# ensure => directory,
+# owner => 'root',
+# group => 'root',
+# mode => '0775',
+# }
 
 #config default.conf
-file {'/etc/nginx/conf.d/default.conf':
+file {"${configdir}/conf.d/default.conf":
 ensure => file,
-source => 'puppet:///modules/nginx/default.conf',
-require => Package['nginx'],
+content => template('nginx/default.conf.erb'),
+# source => 'puppet:///modules/nginx/default.conf',
+# require => Package['nginx'],
 notify => Service['nginx'],
 }
 
